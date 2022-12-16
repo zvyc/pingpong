0cb69ffcc8ee815c480586a865b1bfd6b46cc7f9
Erik <erik32x@protonmail.com>
2022-12-16 15:21:55 +0100

    Responsive navbar
    Moved the navbar outside the header element for it to be able to stick to the top of the page everywhere. Also make the navbar shrink when scrolling

--- js/global.js
+++ js/global.js
@@ -9,8 +9,29 @@
 
   Drupal.behaviors.pingpong = {
     attach: function (context, settings) {
-
     }
   };
 
 })(jQuery, Drupal);
+
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".masthead").classList.add("masthead-min");
+    document.querySelector(".navbar-brand img").classList.add("logo-min");
+  } else {
+    document.querySelector(".masthead").classList.remove("masthead-min");
+    document.querySelector(".navbar-brand img").classList.remove("logo-min");
+  }
+}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.querySelector(".masthead").style.marginTop = "4.7rem";
+//     document.querySelector(".navbar-brand img").style.width = "40%";
+//   } else {
+//     document.querySelector(".masthead").style.marginTop = "9rem";
+//     document.querySelector(".navbar-brand img").style.width = "";
+//   }
+// }
---
+++ js/script.js
@@ -0,0 +1,19 @@
+// If the toolbar is open, offset the navbar by its height
+if (document.querySelector("#toolbar-bar")) {
+  document.querySelector("#navbar-top").style.top = document.querySelector('#toolbar-bar').offsetHeight.toString() + "px";
+}
+
+
+// Shirnk the navbar when scrolling
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    // document.querySelector(".masthead").classList.add("masthead-min");
+    document.querySelector(".navbar-brand img").classList.add("logo-min");
+  } else {
+    // document.querySelector(".masthead").classList.remove("masthead-min");
+    document.querySelector(".navbar-brand img").classList.remove("logo-min");
+    // document.querySelector("#navbar-top").style.top = "0";
+  }
+}
