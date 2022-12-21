9f892cca0d3e8ed59f8cb83e8eaab73670c86d28
Erik <erik32x@protonmail.com>
2022-12-21 01:45:53 +0100

    Footer + Open sans font
    Made a basic footer, also switched the main font to Open Sans

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+// If the toolbar is open, offset the navbar by its height (On small screens the body does not contain the class toolbar fixed, if so we do not offset the navbar)
+// if (document.querySelector("#toolbar-bar") && document.querySelector("body").classList.contains("toolbar-fixed")) {
+
 // If the toolbar is open, offset the navbar by its height
-if (document.querySelector("#toolbar-bar")) {
+if (document.querySelector("#toolbar-bar") && document.querySelector("body").classList.contains("toolbar-fixed")) {
   document.querySelector("#navbar-top").style.top = document.querySelector('#toolbar-bar').offsetHeight.toString() + "px";
 }
 
@@ -8,7 +11,7 @@ if (document.querySelector("#toolbar-bar")) {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if ((document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) && (document.body.scrollHeight >= 790)) {
     // document.querySelector(".masthead").classList.add("masthead-min");
     document.querySelector(".navbar-brand img").classList.add("logo-min");
   } else {
