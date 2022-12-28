ddb96aebc2ca0ecdb9b9be74cd2ce2d65a8691d0
Erik <erik32x@protonmail.com>
2022-12-28 23:30:32 +0100

    Big cleanup. Removed unused files and comments
    Also restructured some code and made minor tweaks to the styling of the website

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pingpong
-Drupal subtheme project
+Drupal subtheme for barrio project
+
+Created for Stenungsunds BTF, a local table tennis club
+
+The theme is responsive and mobile friendly
+
+A completed site using the subtheme can be found at https://stenungsundsbtf.se
--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// If the toolbar is open, offset the navbar by its height (On small screens the body does not contain the class toolbar fixed, if so we do not offset the navbar)
-// if (document.querySelector("#toolbar-bar") && document.querySelector("body").classList.contains("toolbar-fixed")) {
-
 // If the toolbar is open, offset the navbar by its height
 if (document.querySelector("#toolbar-bar") && document.querySelector("body").classList.contains("toolbar-fixed")) {
   document.querySelector("#navbar-top").style.top = document.querySelector('#toolbar-bar').offsetHeight.toString() + "px";
@@ -12,11 +9,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if ((document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) && (document.body.scrollHeight >= 790)) {
-    // document.querySelector(".masthead").classList.add("masthead-min");
     document.querySelector(".navbar-brand img").classList.add("logo-min");
   } else {
-    // document.querySelector(".masthead").classList.remove("masthead-min");
     document.querySelector(".navbar-brand img").classList.remove("logo-min");
-    // document.querySelector("#navbar-top").style.top = "0";
   }
 }
